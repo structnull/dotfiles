55beb067f117ce76755f703d2d2b9cd274c3ba35
chiefdinkan <rahulrex123456789@gmail.com>
2023-01-15 23:33:42 +0530

    wf-recorder script

---
+++ .scripts/record-screen.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pid=`pgrep wf-recorder`
+status=$?
+
+if [ $status != 0 ]; then
+	 wf-recorder --audio=alsa_output.pci-0000_00_1f.3.analog-stereo.monitor -c h264_vaapi -d /dev/dri/renderD129 -f $(xdg-user-dir VIDEOS)/Screencasts/screencast-$(date +'%Y-%m-%d-%H%M%S.mp4');
+else
+	 pkill --signal SIGINT wf-recorder
+     notify-send "Recording has stopped";
+fi;
+
