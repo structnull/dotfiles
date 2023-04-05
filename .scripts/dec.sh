89c0925bd120de22302857af0e63886a5ea330e7
chiefdinkan <rahulrex123456789@gmail.com>
2023-04-05 23:06:10 +0530

    added cliphist

--- .scripts/dec.sh
+++ .scripts/dec.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #
-HYPRGAMEMODE=$(hyprctl getoption animations:enabled | sed -n '2p' | awk '{print $2}')
-if [ $HYPRGAMEMODE = 1 ] ; then
+HYPRPERFMODE=$(hyprctl getoption animations:enabled | sed -n '2p' | awk '{print $2}')
+if [ $HYPRPERFMODE = 1 ] ; then
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
         keyword decoration:blur 0;\
         keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+        keyword decoration:rounding 0;\
+        keyword monitor eDP-1,1920x1080@60,0x0,1"
     exit
 fi
 hyprctl reload
