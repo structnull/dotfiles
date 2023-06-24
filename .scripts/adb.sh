f9f5a41769c41a8d8e8796766aac2681102e513b
chiefdinkan <rahulrex123456789@gmail.com>
2023-06-24 22:15:59 +0530

    adb rework

--- .scripts/adb.sh
+++ .scripts/adb.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-
 if ! command -v adb &> /dev/null; then
-    echo "ADB is not installed. Please install ADB and try again."
-    exit 1
+	echo "ADB is not installed. Please install ADB and try again."
+	exit 1
 fi
 
 device=$(adb devices | awk 'NR==2 {print $1}')
 if [ -z "$device" ]; then
-    echo "No device found. Connect a device and try again."
-    exit 1
+	echo "No device found. Connect a device and try again."
+	exit 1
 fi
 
 ip_address=$(adb -s $device shell ip route | awk 'NR==1 {print $9}')
 
 if [ -z "$ip_address" ]; then
-    echo "Failed to retrieve IP address. Make sure the device is connected and try again."
-    exit 1
+	echo "Failed to retrieve IP address. Make sure the device is connected and try again."
+	exit 1
 fi
 
 adb -s $device tcpip 5555
 adb -s $device connect $ip_address:5555
 
-echo "Device connected via Wi-Fi."
+# Check if the connection is established
+connected_devices=$(adb devices | awk 'NR>1 {print $1}')
+if [[ $connected_devices =~ (^|[[:space:]])"$device"($|[[:space:]]) ]]; then
+	echo "Device connected via Wi-Fi."
+else
+	echo "Failed to establish a Wi-Fi connection with the device."
+fi
