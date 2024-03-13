339c987059cc6a996537b9aa250763f679036243
chiefdinkan <rahulrex123456789@gmail.com>
2024-03-13 16:42:03 +0530

    huh

--- .scripts/grim_ocr.sh
+++ .scripts/grim_ocr.sh
@@ -1,15 +1,18 @@
 #!/bin/bash 
 # Dependencies: tesseract-ocr imagemagick scrot xsel/x11 wl-clipboard/wayland
 
-SCR_IMG=`mktemp`
-trap "rm $SCR_IMG*" EXIT
+SCR_IMG=$(mktemp)
+trap 'rm -f "$SCR_IMG"*' EXIT
 
-grim -l 9 -g "$(slurp)" $SCR_IMG.png 
-# increase image quality with option -q from default 75 to 100
+# Capture screenshot with grim, increase image quality with option -q from default 75 to 100
+grim -l 9 -g "$(slurp)" "$SCR_IMG.png"
 
-mogrify -modulate 100,0 -resize 400% $SCR_IMG.png 
+# Enhance the image quality before OCR
+mogrify -modulate 100,0 -resize 400% "$SCR_IMG.png"
 
-tesseract $SCR_IMG.png $SCR_IMG &> /dev/null
-cat $SCR_IMG.txt | wl-copy 
+# Use Tesseract to perform OCR on the screenshot, discard stderr
+tesseract "$SCR_IMG.png" "$SCR_IMG" 2>/dev/null
+
+wl-copy < "$SCR_IMG.txt"
 
 exit
