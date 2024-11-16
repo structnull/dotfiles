8b7cda2c85691e931c2c621f40abb673b83a4fab
structnull <rahulrex123456789@gmail.com>
2024-11-16 11:53:43 +0530

    volt

---
+++ .config/nvim/nvim/nvim/lua/plugins/init.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = { "rust" },
+    dependencies = "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.rustaceanvim"
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "json",
+        "toml",
+        "c",
+        "markdown",
+        "python",
+        "cpp",
+        "rust",
+        "bash",
+        "typescript",
+      },
+    },
+  },
+}
