c4a2c7bf1b0bafcd175c8597027ad466a13dc352
structnull <adharshva321@gmail.com>
2024-12-16 00:49:59 +0530

    new waybar conf

---
+++ init.lua
@@ -0,0 +1,24 @@
+vim.o.number = true
+vim.o.showmode = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.termguicolors = true 
+vim.o.background = "dark" 
+vim.o.scrolloff = 8 
+vim.o.sidescrolloff = 8 
+vim.o.cursorlineopt = "both" 
+vim.opt.whichwrap:append("<,>,h,l")
+vim.o.hlsearch = true 
+vim.o.incsearch = true 
+vim.o.ignorecase = true 
+vim.o.smartcase = true 
+vim.o.hidden = true 
+vim.o.laststatus = 2 
+vim.o.showtabline = 2 
+vim.o.mouse = "a" 
+vim.o.completeopt = "menuone,noinsert,noselect"
+vim.cmd("syntax enable")
+vim.opt.clipboard:append("unnamedplus")
