68b2a36047b4fb5410a5e9ce77bf53f961ed47bc
structnull <adharshva321@gmail.com>
2025-01-07 22:11:34 +0530

    okda

--- .config/nvim/lua/plugins/myplugins.lua
+++ .config/nvim/lua/plugins/myplugins.lua
@@ -2,39 +2,39 @@ local overrides = require "configs.overrides"
 
 return {
 
-  ----------------------------------------- default plugins ------------------------------------------
+	----------------------------------------- default plugins ------------------------------------------
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        -- format & linting
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "nvchad.configs.lspconfig"
-      require "configs.lspconfig"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				-- format & linting
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require "configs.null-ls"
+				end,
+			},
+		},
+		config = function()
+			require "nvchad.configs.lspconfig"
+			require "configs.lspconfig"
+		end,
+	},
 
-  --------------------------------------------- custom plugins ----------------------------------------------
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^4", -- Recommended
-    lazy = false, -- This plugin is already lazy
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "eandrju/cellular-automaton.nvim",
-    lazy = false,
-  },
-  { "nvzone/volt", lazy = true },
-  { "nvzone/menu", lazy = true },
+	--------------------------------------------- custom plugins ----------------------------------------------
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"eandrju/cellular-automaton.nvim",
+		lazy = false,
+	},
+	{ "nvzone/volt", lazy = true },
+	{ "nvzone/menu", lazy = true },
 }
