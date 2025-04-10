13a6a2c321ae87e20b9c605096756f03ded307af
structnull <adharshva321@gmail.com>
2025-04-10 23:23:31 +0530

    update

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,49 +8,49 @@ map("n", "<C-j>", ":TmuxNavigateDown<CR><ESC>", { desc = "tmux_navigator Window 
 map("n", "<C-k>", ":TmuxNavigateUp<CR><ESC>", { desc = "tmux_navigator Window left" })
 
 map("n", "<leader>tw", function()
-	vim.wo.wrap = not vim.wo.wrap
+  vim.wo.wrap = not vim.wo.wrap
 end, {
-		desc = "Toggle wrap",
-	})
+  desc = "Toggle wrap",
+})
 
 map("n", "<leader>tt", function()
-	vim.g.transparency_enabled = not vim.g.transparency_enabled
-	require("base46").toggle_transparency()
-	if vim.g.transparency_enabled then
-		vim.notify("Toggled On", vim.log.levels.INFO, { title = "Transparency" })
-	else
-		vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Transparency" })
-	end
+  vim.g.transparency_enabled = not vim.g.transparency_enabled
+  require("base46").toggle_transparency()
+  if vim.g.transparency_enabled then
+    vim.notify("Toggled On", vim.log.levels.INFO, { title = "Transparency" })
+  else
+    vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Transparency" })
+  end
 end, { desc = "Options | Toggle Transparency", silent = true })
 
 map("n", "<leader>cc", function()
-	local file = vim.fn.expand "%:t"
-	local fp = vim.fn.expand "%:p:h"
-	local ft = vim.bo.ft
-	local supported_ft = { "python", "cpp", "c" }
+  local file = vim.fn.expand "%:t"
+  local fp = vim.fn.expand "%:p:h"
+  local ft = vim.bo.ft
+  local supported_ft = { "python", "cpp", "c" }
 
-	if not vim.tbl_contains(supported_ft, ft) then
-		print "Error: Unsupported file type"
-		return
-	end
+  if not vim.tbl_contains(supported_ft, ft) then
+    print "Error: Unsupported file type"
+    return
+  end
 
-	require("nvchad.term").runner {
-		id = "run",
-		pos = "sp",
-		cmd = function()
-			local ft_cmds = {
-				python = "python3 " .. file,
-				cpp = "clear && g++ -o out " .. file .. " && ./out",
-				c = "clear && gcc -o out " .. file .. " && ./out",
-			}
-			return "cd " .. fp .. " && clear && " .. ft_cmds[ft]
-		end,
-	}
+  require("nvchad.term").runner {
+    id = "run",
+    pos = "sp",
+    cmd = function()
+      local ft_cmds = {
+        python = "python3 " .. file,
+        cpp = "clear && g++ -o out " .. file .. " && ./out",
+        c = "clear && gcc -o out " .. file .. " && ./out",
+      }
+      return "cd " .. fp .. " && clear && " .. ft_cmds[ft]
+    end,
+  }
 end, { desc = "Code Runner" })
 
 -- Keyboard users
 vim.keymap.set("n", "<C-t>", function()
-  require("menu").open("default")
+  require("menu").open "default"
 end, {})
 
 -- mouse users + nvimtree users!
@@ -62,9 +62,9 @@ vim.keymap.set("n", "<RightMouse>", function()
 end, {})
 
 for i = 1, 9, 1 do
-	vim.keymap.set("n", string.format("<A-%s>", i), function()
-		vim.api.nvim_set_current_buf(vim.t.bufs[i])
-	end)
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
 end
 
 for i = 1, 9, 1 do
@@ -75,5 +75,27 @@ end
 
 -- Call "CellularAutomaton make_it_rain" command
 map("n", "<leader>mk", "<cmd>CellularAutomaton make_it_rain<CR>", {
-	desc = "make it rain",
+  desc = "make it rain",
 })
+
+-- Global variable to track inline diagnostic virtual text state (off by default)
+_G.diagnostics_virtual_text = false
+
+-- Toggle function for inline diagnostics (virtual_text)
+local function toggle_diagnostics()
+  _G.diagnostics_virtual_text = not _G.diagnostics_virtual_text
+  vim.diagnostic.config({
+    virtual_text     = _G.diagnostics_virtual_text,
+    signs            = true,
+    underline        = true,
+    update_in_insert = false,
+  })
+  vim.notify(
+    _G.diagnostics_virtual_text and "Inline diagnostics enabled" or "Inline diagnostics disabled",
+    vim.log.levels.INFO,
+    { title = "Diagnostics" }
+  )
+end
+
+-- Map <leader>s to toggle inline diagnostics
+map("n", "<leader>s", toggle_diagnostics, { desc = "Toggle inline diagnostics", silent = true })
