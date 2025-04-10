ea08b09426c27161e560357e418da469a3555b6b
structnull <adharshva321@gmail.com>
2025-04-10 22:58:29 +0530

    made stuff

--- .config/nvim/lua/plugins/myplugins.lua
+++ .config/nvim/lua/plugins/myplugins.lua
@@ -6,15 +6,6 @@ return {
 
 	{
 		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				-- format & linting
-				"jose-elias-alvarez/null-ls.nvim",
-				config = function()
-					require "configs.null-ls"
-				end,
-			},
-		},
 		config = function()
 			require "nvchad.configs.lspconfig"
 			require "configs.lspconfig"
