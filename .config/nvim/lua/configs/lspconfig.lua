c004ee68bdb17f955193d74418b5aa120382784b
structnull <adharshva321@gmail.com>
2025-05-10 04:22:26 +0530

    neo

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,25 +1,13 @@
-local nvlsp = require "nvchad.configs.lspconfig"
-local lspconfig = require "lspconfig"
-
-nvlsp.defaults() -- loads nvchad's defaults
-
 local servers = {
   "clangd",
   "html",
-  "pyright",
-  "astro",
-  "ts_ls",
+  "ruff",
+  "oxlint",
   "cssls",
+  "rust_analyzer",
   "marksman",
   "jsonls",
 }
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+vim.lsp.enable(servers)
 
