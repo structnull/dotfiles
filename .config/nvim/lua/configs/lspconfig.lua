91025ef7183bbf01f23b496e558e478488a80a5e
structnull <adharshva321@gmail.com>
2025-05-11 09:46:42 +0530

    flutterrrrrrrrrrrrrrrrrrrrr(2)

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,3 +1,4 @@
+require("nvchad.configs.lspconfig").defaults()
 local servers = {
   "clangd",
   "html",
@@ -5,12 +6,29 @@ local servers = {
   "pyright",
   "oxlint",
   "cssls",
+  "html",
   "rust_analyzer",
   "marksman",
   "jsonls",
 }
 
 vim.lsp.enable(servers)
+
+local function on_attach(_, bufnr)
+  local map = vim.keymap.set
+  local opts = { buffer = bufnr }
+
+  map("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+  map("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+  map("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+  map("n", "gs", "<cmd>Telescope lsp_document_symbols<CR>", opts)
+  map("n", "gS", "<cmd>Telescope lsp_workspace_symbols<CR>", opts)
+  map("n", "gy", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+end
+
+vim.lsp.config("*", {
+  on_attach = on_attach,
+})
 
 vim.lsp.config("pyright", {
   settings = {
