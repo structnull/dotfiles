3c890fcfde1d1fcebe87389ff0ca09b400fa2f2c
structnull <adharshva321@gmail.com>
2025-11-20 19:24:31 +0530

    updates

--- .scripts/.scripts/battery.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bash -c "echo 2 > /sys/devices/platform/asus-nb-wmi/throttle_thermal_policy"
--- .scripts/.scripts/start-windows.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gfx_mode=$(supergfxctl -g)
-
-if [ "$gfx_mode" = "Hybrid" ]; then
-    notify-send -u critical "Error" "Switch to integrated first!!"
-else
-    sh -c "virsh --connect=qemu:///system start win10; sleep 13; looking-glass-client -F"
-fi
