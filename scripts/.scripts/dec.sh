460f11463cf24943a12aca491255ebf97def494d
structnull <adharshva321@gmail.com>
2026-07-14 22:26:47 +0530

    updated .dec

--- scripts/.scripts/dec.sh
+++ scripts/.scripts/dec.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env sh
-HYPRGAMEMODE=$(hyprctl getoption animations.enabled | awk 'NR==1{print $NF}')
-case "$HYPRGAMEMODE" in
-1|true|yes|on)
-    hyprctl eval 'hl.config({
-        animations = { enabled = false },
-        decoration = {
-            shadow = { enabled = false },
-            blur = {
-                enabled = false,
-                ignore_opacity = false,
+
+GAMEMODE_STATE=$(hyprctl -j getoption animations:enabled | jq '.bool')
+
+if [ "$GAMEMODE_STATE" = "true" ]; then
+    hyprctl eval '
+        hl.config({
+            animations = { enabled = false },
+            decoration = {
+                shadow = { enabled = false },
+                blur = {
+                    enabled = false,
+                    ignore_opacity = false,
+                },
+                rounding = 0,
             },
-            rounding = 0,
-        },
-        general = { border_size = 1 },
-    })'
+            general = {
+                border_size = 1,
+            },
+        })
+        hl.monitor({
+            output = "",
+            mode = "1920x1080@60.00",
+            position = "auto",
+            scale = 1,
+        })
+    '
     exit
-    ;;
-esac
+fi
+
 hyprctl reload
